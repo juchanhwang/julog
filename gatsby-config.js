92ea9f5eb2417fff1ac7348ed04653813bf5cfff
juchanhwang <ghkdwncks96@gmail.com>
2020-05-30 02:30:12 +0900

    feat(my-blog-starter): add google analytics trackingId

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
+        // The property ID; the tracking code won't be generated without it
         trackingId: metaConfig.ga,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
       },
     },
     {
