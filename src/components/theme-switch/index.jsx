22496fc4a412465c7e6c48c78d45b03a57dc6871
juchanhwang <ghkdwncks96@gmail.com>
2020-06-12 19:51:24 +0900

    refactor(theme-switch/index): set theme dark mode

--- src/components/theme-switch/index.jsx
+++ src/components/theme-switch/index.jsx
@@ -7,7 +7,7 @@ import { THEME } from '../../constants'
 import './index.scss'
 
 function getTheme(checked) {
-  return checked ? THEME.LIGHT : THEME.DARK;
+  return checked ? THEME.DARK : THEME.LIGHT
 }
 
 function toggleTheme(theme) {
@@ -36,7 +36,7 @@ export const ThemeSwitch = () => {
   }
 
   useEffect(() => {
-    const checked = Dom.hasClassOfBody(THEME.DARK)
+    const checked = true
 
     handleChange(checked)
   }, [])
